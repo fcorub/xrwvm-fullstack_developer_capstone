521c797b367b049a70e6164f500608c065af875d
fcorub <fco.ruben.n.c@gmail.com>
2025-11-10 11:05:58 -0500

    Added CarModel and CarMake Django models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
-# from django.contrib import admin
-# from .models import related models
-
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of extra empty forms to display
 
 # CarModelAdmin class
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Fields to show in CarMake list view
+    inlines = [CarModelInline]  
 
 # CarMakeAdmin class with CarModelInline
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
